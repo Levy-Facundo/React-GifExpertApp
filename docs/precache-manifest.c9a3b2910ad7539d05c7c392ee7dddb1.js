c68cfbc9efa7a6230c48483558c262273768005e
Levy Facundo <levy.facundo.01@gmail.com>
2020-07-29 20:18:24 -0300

    docs agregado1

---
+++ docs/precache-manifest.c9a3b2910ad7539d05c7c392ee7dddb1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0e701412dfb0ae57a594d1d9dde30f80",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cafd5edf148d99835a37",
+    "url": "/static/css/main.8e22e3d0.chunk.css"
+  },
+  {
+    "revision": "3246fd5ee62fe446f3fe",
+    "url": "/static/js/2.5c2b00ab.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5c2b00ab.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cafd5edf148d99835a37",
+    "url": "/static/js/main.0cba4a6c.chunk.js"
+  },
+  {
+    "revision": "9d3929294d677b1b9ff6",
+    "url": "/static/js/runtime-main.aa9df6a3.js"
+  }
+]);
